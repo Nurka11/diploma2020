0021dd13f7a0a8c77a0c9c3e2f7146da8af170a9
Nurzhan <ormanali99@gmail.com>
2020-05-13 18:15:40 +0600

    update

--- app/monitoring/urls.py
+++ app/monitoring/urls.py
@@ -3,12 +3,10 @@ from django.urls import path
 from django.conf import settings
 from django.urls import include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mon_app.urls'))
 ]
-
 
 if settings.DEBUG:
     import debug_toolbar
